ae7135c0efe199aa5a52c8a3944d89aa78d94dcd
saintsun20 <azizgunes@msn.com>
2024-10-07 21:12:30 +0300

    Login İşlemi hem classta hem cucumber feature da yapıldı

---
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -0,0 +1,22 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.*; // * hepsini kapsıyor
+
+public class _01_LoginSteps {
+
+    @Given("Navigate to Campus")
+    public void navigate_to_campus() {
+
+        System.out.println("Site açıldı");
+    }
+    @When("Enter username and password and click login button")
+    public void enter_username_and_password_and_click_login_button() {
+
+        System.out.println("Username ve şifre gönderildi.");
+    }
+    @Then("User should login successfully")
+    public void user_should_login_successfully() {
+
+        System.out.println("Siteye girildiği doğrulandı");
+    }
+}
